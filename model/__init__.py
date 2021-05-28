7b856ac49c1edea9eabcbdf94c82b874993f7f73
XinyueWei <13659185706@163.com>
2021-05-28 09:10:00 -0700

    update BN frozen

--- model/__init__.py
+++ model/__init__.py
@@ -12,9 +12,11 @@ def CreateModel(args):
         assert args.init_weight is not None
         ckpt = torch.load(args.init_weight)
         model.load_state_dict(ckpt['state_dict'])
-        for m in model.modules():
-            if isinstance(m, nn.BatchNorm2d):
-                print("frozen")
+        encoders = ["pconv1","pconv2","pconv3","pconv4","pconv5","pconv6","pconv7","pconv8"]
+        for name, m in model.named_modules():
+            name = name.split('.')[0]
+            if name in encoders and isinstance(m, nn.BatchNorm2d):
+                print(name, "frozen")
                 m.eval()
                 # shutdown update in frozen mode
                 m.weight.requires_grad = False
